bcfb714d4abfd746aa55b6464f30825754cc9e3f
mirkom117 <mirko.montani@studenti.unicam.it>
2025-09-22 19:42:42 +0200

    Create "CreateEventAction" Class

---
+++ FilieraAgricola/FilieraAgricola/src/it/unicam/cs/ids/filieraagricola/system/CreateEventAction.java
@@ -0,0 +1,8 @@
+package it.unicam.cs.ids.filieraagricola.system;
+
+public class CreateEventAction implements  SystemAction{
+    @Override
+    public void handleRequest(FormatRequest request) {
+
+    }
+}
