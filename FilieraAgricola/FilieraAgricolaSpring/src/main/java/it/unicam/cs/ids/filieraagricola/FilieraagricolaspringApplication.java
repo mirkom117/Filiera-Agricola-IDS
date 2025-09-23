8feda6f74d2489a948661d19b9602ef846649b83
mirkom117 <mirko.montani@studenti.unicam.it>
2025-09-23 10:28:03 +0200

    Setup Spring Project

---
+++ FilieraAgricola/FilieraAgricolaSpring/src/main/java/it/unicam/cs/ids/filieraagricola/FilieraagricolaspringApplication.java
@@ -0,0 +1,13 @@
+package it.unicam.cs.ids.filieraagricola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilieraagricolaspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilieraagricolaspringApplication.class, args);
+	}
+
+}
