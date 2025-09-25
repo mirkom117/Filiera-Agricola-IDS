c7d477758003e925953181cc39712d6a41052d79
) <62116880+menestresser@users.noreply.github.com>
2025-09-25 20:55:52 +0200

    Adding Enum

---
+++ FilieraAgricola/FilieraAgricolaSpring/src/main/java/it/unicam/cs/ids/filieraagricola/model/ParticipationRole.java
@@ -0,0 +1,11 @@
+package it.unicam.cs.ids.filieraagricola.model;
+
+/**
+ * Roles available for a {@link Participation} in an {@link Event}.
+ */
+public enum ParticipationRole {
+    ORGANIZER,
+    EXHIBITOR,
+    SPEAKER,
+    VISITOR
+}
