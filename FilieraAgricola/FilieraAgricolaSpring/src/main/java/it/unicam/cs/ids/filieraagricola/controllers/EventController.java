9dab33989b0852b7ccba4f7d5e401c0a7bf704fe
Ennio Lizza <enni.lizza@gmail.com>
2025-09-25 18:41:14 +0200

    Updating EventController

--- FilieraAgricola/FilieraAgricolaSpring/src/main/java/it/unicam/cs/ids/filieraagricola/controllers/EventController.java
+++ FilieraAgricola/FilieraAgricolaSpring/src/main/java/it/unicam/cs/ids/filieraagricola/controllers/EventController.java
@@ -1,7 +1,9 @@
 package it.unicam.cs.ids.filieraagricola.controllers;
 
 import it.unicam.cs.ids.filieraagricola.model.Event;
+import it.unicam.cs.ids.filieraagricola.model.UserRole;
 import it.unicam.cs.ids.filieraagricola.services.EventService;
+import it.unicam.cs.ids.filieraagricola.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,13 +12,19 @@ import java.util.List;
 @RestController
 @RequestMapping("/events")
 public class EventController {
-
+    @Autowired
+    UserService userService;
 
     @Autowired
     private EventService service;
 
     @PostMapping
     public Event organizeEvent(@RequestBody Event event) {
+        //Animator
+        if (!userService.hasRole(UserRole.ANIMATOR)) {
+            // restituire codice http
+            return null;
+        }
         return service.organizeEvent(event);
     }
 
